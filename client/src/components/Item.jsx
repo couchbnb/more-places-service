a356901c826050cae1e95d8e3bf1c2a8da72d5ce
codingfox <carinalxx@gmail.com>
2020-09-30 09:43:57 -0700

    heart button added

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Slider from 'react-slick';
+import Heart from './Heart.jsx';
 
 const Item = ({ item }) => {
   const {
@@ -33,8 +33,7 @@ const Item = ({ item }) => {
     border-bottom-left-radius: 12px;
     border-bottom-right-radius: 12px;
     pointer-events: none;
-    overflow: hidden;
-    
+    overflow: hidden; 
   `;
   return (
     <div>
@@ -43,33 +42,9 @@ const Item = ({ item }) => {
           <div>SuperHost</div>
         </Upleft>
         <Upright>
-          <div>
-            <svg
-              type="button"
-              viewBox="0 0 32 32"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-              role="presentation"
-              focusable="false"
-              style={{
-                display: 'block',
-                fill: 'rgb(0, 0, 0, 0.5)',
-                height: '24px',
-                width: '24px',
-              }}
-              stroke="currentcolor"
-              strokeWidth="2"
-              overflow="visible"
-            >
-              <path d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791
-            0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949
-            2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"
-              />
-            </svg>
-          </div>
+          <Heart />
         </Upright>
         <Pic src={picture} />
-
       </Frame>
       <span>{star}</span>
       <span>
@@ -85,11 +60,3 @@ const Item = ({ item }) => {
 };
 
 export default Item;
-
-//   const Wrapper = styled.section`
-
-//     padding: 4em;
-//     border-width: 0px 6px;
-//     max-width: 66.6667%;
-//     flex: 0 0 66.6667%;
-//   `;
--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import Item from './Item.jsx';
-// import Slider from 'react-slick';
 
 const List = ({ data }) => {
   const Slide = styled.div`
@@ -16,14 +15,8 @@ const List = ({ data }) => {
   scroll-snap-type: x mandatory !important;
   `;
 
-  // const setting = {
-  //   infinite: true,
-  //   speed: 500,
-  //   slidesToShow: 4,
-  //   slidesToScroll: 4,
-  // };
+ 
   return (
-    // <Slider {...settings}>
 
     <Slide>
       {data.map((item, index) => <Item item={item} key={index} />)}
